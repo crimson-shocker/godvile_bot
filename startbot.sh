b687a6b280e5f213b7fef36d173e287fbfb731b3
baltazarbd <baltazarbd@rambler.ru>
2016-05-11 17:16:15 +0300

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+user_ip=`cat ./passwd|grep ip|awk'{print $2}'`
 case $1 in
 
   "-install")
@@ -22,6 +23,7 @@ case $1 in
     echo `date +%F_%H:%M:%S` Start Bot  >>/var/log/bot.log
     echo +++++++++++++++++++++++++++++++++++++++++++++++++++++ >>/var/log/bot.log
     /usr/bin/docker run  --rm  -p 94.141.168.73:5901:5901  --name godvile -v /opt/godvile_bot/:/opt/ bot /opt/godville.py >>/var/log/bot.log
+    #/usr/bin/docker run  --rm  -p $user_ip:5901:5901  --name godvile -v /opt/godvile_bot/:/opt/ bot /opt/godville.py >>/var/log/bot.log
     echo +++++++++++++++++++++++++++++++++++++++++++++++++++++ >>/var/log/bot.log
     echo `date +%F_%H:%M:%S` Stop Bot  >>/var/log/bot.log
     echo +++++++++++++++++++++++++++++++++++++++++++++++++++++ >>/var/log/bot.log
